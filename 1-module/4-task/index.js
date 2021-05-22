87126c96c21df766f6e55e2b534c3320d19258e8
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-05-22 23:57:26 +0300

    Задача 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  if (((str.toLowerCase()).indexOf("1xbet") != -1) || ((str.toLowerCase()).indexOf("xxx") != -1)){
+      return true
+  }
+  else return false
 }
