ac67bceda9a15b789c0b943060125d6e9d9fa4a6
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-06-05 20:24:06 +0300

    Задача 2.3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  x: 0,
+  y: 0,
+
+  read(a,b){
+    this.x = a;
+    this.y = b;
+  },
+
+  sum(){
+    return this.x + this.y;
+  },
+  mul(){
+    return this.x * this.y;
+  }
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
