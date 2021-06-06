87eb845a14efbe46229ab243b7663ec0f550790c
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-06-06 18:09:03 +0300

    Задача 3.2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,15 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let array = [];
+  for (let key in arr){
+    if (arr[key] == a) {
+      array.push(arr[key]);
+    }
+    else if ((arr[key] > a) && (arr[key] < b)){
+      array.push(arr[key]);
+    }
+    if (arr[key] == b) {
+      array.push(arr[key]);
+    }
+  }
+  return array
 }
