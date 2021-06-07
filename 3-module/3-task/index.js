5eb8691c99124be6f10fc15c2df21410a4f90291
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-06-07 19:51:15 +0300

    Задача 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '') {
+      if (!isNaN(Number(arr[i]))) {
+        return '';
+      } else {
+        if (i > 0) {
+          arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1, arr[i].length);
+        }
+      }
+    }
+  }
+  return arr.join('');
 }
