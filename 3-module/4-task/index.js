13f63b065c38ed9a70332a57baccc0974f7e283b
vladimir.adamchuk <vldmr.adamchuk@gmail.com>
2021-06-07 20:20:29 +0300

    Задача 3.4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let ex = '';
+  let arr = [];
+  for (let key in users){
+      if ((users[key].age < age) || (users[key].age == age)){
+        ex = users[key].name + ', ' + users[key].balance;
+        arr.push(ex);
+      }
+  }
+  return arr.join('\n');
 }
